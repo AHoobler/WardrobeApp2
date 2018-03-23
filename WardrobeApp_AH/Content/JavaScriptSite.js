d04bdfb47cf97421242e9c66b3f4246f8a37de44
AHoobler <ashley.hoobler@gmail.com>
2018-03-23 15:31:57 -0400

    add name function

--- WardrobeApp_AH/Content/JavaScriptSite.js
+++ WardrobeApp_AH/Content/JavaScriptSite.js
@@ -32,3 +32,42 @@ function Redirect(x) {
         win.focus();
     }
 }
+
+function GetGame() {
+    var gameArray = new Array(0);
+    var userResponse = confirm('Would you like to play a game?');
+    while (userResponse) {
+        var guess = prompt('Please guess Dobbys favorite clothing item');
+        userResponse = confirm('Would you like to add another guess');
+        gameArray.push(guess);
+       
+    }
+   
+    if (guess == 'sock') {
+        
+        getElementById('gameresult').innertext = 'You have guessed Dobbys favorite clothing item!';
+    }
+
+    else if (gameArray != 'sock') {
+        getElementById('gameresult').innertext = 'You lose the game.';
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function FUNCTION() {
+    var x = document.getElementById("mainsection");
+    var userResponse = prompt('Welcome to Dobbys Closet. What is your name?');
+    x.getElementsByTagName('h1')[0].innerText = 'Welcome to Dobbys Closet ' + userResponse;
+}
